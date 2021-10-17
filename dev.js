9536692275b7c273afb93123c01f2919ff0be6c0
devzfz <83339009+devzfz@users.noreply.github.com>
2021-10-17 14:13:16 +0700

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,2 +1,3 @@
 let EFV='<a href="https://nogisub.blogspot.com" style="position:absolute;z-index:-9999;left:-100%;">nogisub</a><a href="https://www.nogisub.com" style="position:absolute;z-index:-9999;left:-100%;">Devzfz</a>'
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
+Devzfz
